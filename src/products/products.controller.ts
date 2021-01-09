eb1a4960dbf4f40431976fc85a01e7e838351084
@alexander.shatravka1 <alexander.shatravka@gmail.com>
2021-01-09 13:20:38 +0200

    setting up module for products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,30 +6,57 @@ import {
   Delete,
   Put,
   Body,
+  Redirect,
+  HttpCode,
+  HttpStatus,
+  Header,
+  Req,
+  Res,
 } from '@nestjs/common';
 
-import { CreateProductDto } from './dto/create-product.dto';
+import { CreateProductDto } from './dto/create-product.dto';2
+import { UpdateProductDto } from './dto/update-product.dto';
+
+// import { Request, Response } from 'express';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  // Below example of Redirect and Express handling requests
+  //
+  // @Get()
+  // @Redirect('https://google.com', 301)
+  // getAll(@Req() req: Request, @Res() res: Response): string {
+  //   res.status(201).end('Bye');
+  //   return 'getAll';
+  // }
+
   @Get()
-  getAll(): string {
-    return 'getAll';
+  getAll() {
+    return this.productsService.getAll();
   }
 
   @Get(':id')
   getOne(@Param('id') id: string): string {
-    return `getOne ${id}`;
+    return `getOne ${this.productsService.getById(id)}`;
   }
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto): string {
-    return `Title: ${createProductDto.title} Price: ${createProductDto.price}`;
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Cache-control', 'none')
+  create(@Body() createProductDto: CreateProductDto): void {
+    return this.productsService.create(createProductDto);
   }
 
   @Delete()
-  remove() {}
+  remove(@Param('id') id: string) {
+    return `Remove ${id}`;
+  }
 
   @Put()
-  update() {}
+  update(@Body() updateProductDto: UpdateProductDto, @Param('id') id: string) {
+    return `Update ${id}`;
+  }
 }
